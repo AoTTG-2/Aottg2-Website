cc8b3ff9004dc44ba542507c6a2cc314e0b8e2a0
Ghegi Jimenez <dev@gisketch.com>
2024-09-19 19:03:43 +0800

    Fix mobile responsiveness layout

--- src/screens/Team/index.tsx
+++ src/screens/Team/index.tsx
@@ -15,15 +15,15 @@ const Team = () => {
   return (
     <BgContainer customSrc={TeamBg}>
       <div className="flex flex-col lg:flex-row gap-12 lg:mt-4 text-gray-100 relative justify-center">
-        <div className="text-center lg:pr-4 font-primary text-2xl lg:text-4xl flex flex-col z-10 items-center gap-0">
+        <div className="text-center lg:pr-4 font-primary flex flex-col z-10 items-center gap-0">
           <span className="flex flex-row items-end justify-center">
-            <h2 className="text-4xl lg:text-6xl">The Team Behind</h2>
+            <h2 className="text-2xl md:text-4xl lg:text-6xl">The Team Behind</h2>
             <img
               src={AotLogo}
-              className="ml-2 lg:ml-4 h-[3rem] lg:h-[4.5rem]"
+              className="ml-2 lg:ml-4 h-[2rem] md:h-[3rem] lg:h-[4.5rem]"
             />
           </span>
-          <span className="font-secondary lg:mt-2 mb-4 lg:mb-8">
+          <span className="font-secondary lg:mt-2 mb-4 lg:mb-8 text-lg md:text-2xl lg:text-4xl">
             Inspired by Feng Lee's Original Creation
           </span>
           <Button hasBg onClick={handleCreditsClick}>
